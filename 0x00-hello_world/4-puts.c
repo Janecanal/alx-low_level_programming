f1b1b723650e0ffff22bad953073419d3423c81b
Janecanal <janecanal04@gmail.com>
2023-06-15 14:46:33 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - This program prints with the puts function in C Program
  * Return: 0 (this shows code success)
  */
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 } 
