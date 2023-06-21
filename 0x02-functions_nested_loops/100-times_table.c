77a81b3f8f25f5b6b272c3e98a016c7c2db395a1
Janecanal <janecanal04@gmail.com>
2023-06-20 20:19:15 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,29 +1,48 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - entry point
- * Description: computes and print faibonacci
- * Return: 0
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return: void
  */
 
-int main(void)
+void print_times_table(int n)
 {
-	int i = 0;
-	long int a = 0, b = 1, next;
-
-	while (i < 50)
+int a = 0, rep, b;
+if (n < 0 || n > 15)
+	return;
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
-
-		if (i < 49)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-			printf(",");
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
 		}
-		i++;
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
-	putchar('\n');
-	return (0);
+	_putchar('\n');
+	a++;
+}
 }
