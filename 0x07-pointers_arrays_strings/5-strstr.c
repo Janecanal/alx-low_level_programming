9c5b5c5442f5e536a96cf744656975287262eb3e
Janecanal <janecanal04@gmail.com>
2023-07-03 10:02:10 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,8 +4,8 @@
  * _strstr - function  that locates a substring
  * @haystack: string to be searched
  * @needle: substring to be located
- * Return: if the substring is located 
- * - a pointer to the beginning of the located substring 
+ * Return: if the substring is located
+ * - a pointer to the beginning of the located substring
  * if the substring is not located - Null
  */
 
