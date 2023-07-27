81fb050924a2b41bdb04b7c9174797bce0ee1405
Janecanal <janecanal04@gmail.com>
2023-07-27 14:17:22 -0700

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,8 +3,11 @@
 
 /**
  * free_list - this function frees a list_t list.
- * @head: the list_t to be freed
- */
+ * @head: the list_t that will be freed
+ *
+ * Return: nothing
+ **/
+
 void free_list(list_t *head)
 {
 	list_t *tmp;
