a890644e045b12a00781cb8260625d7e356da5ca
Janecanal <janecanal04@gmail.com>
2023-07-31 10:33:13 -0700

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ * Return: the number of nodes printed
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
