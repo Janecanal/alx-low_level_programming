542e12aa82d0c2d6a81c4cf3a1d445a1c3850229
Janecanal <janecanal04@gmail.com>
2023-08-01 03:11:39 -0700

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops the header of a linked list
+ * @head: pointer to the firs element in the list
+ *
+ * Return: noting
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data = 0;
+	listint_t *temp;
+
+	if (!*head)
+		return (data);
+	data = (*head)->n;
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+
+	return (data);
+}
